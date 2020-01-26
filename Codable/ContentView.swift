f915adbc43f32f0a530fffbb7a9590d8640b54c1
Waga <wagaodongo@gmail.com>
2020-01-26 14:21:14 +0300

    coddle

--- Codable/ContentView.swift
+++ Codable/ContentView.swift
@@ -8,9 +8,23 @@
 
 import SwiftUI
 
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+}
+
 struct ContentView: View {
+    @State private var user = User(firstName: "Waga", lastName: "Odongo")
+    
     var body: some View {
-        Text("Hello, World!")
+    
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
     }
 }
 
